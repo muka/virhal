e312625ff3791b97d8ec7307c03c2a51465a771c
Luca Capra <luca.capra@gmail.com>
2017-09-04 06:54:14 +0200

    added service impl

--- api/api.go
+++ api/api.go
@@ -1,1 +1,30 @@
 package api
+
+import (
+	"golang.org/x/net/context"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+type projectService struct{}
+
+func newProjectService() ProjectServiceServer {
+	return new(projectService)
+}
+
+var defaultResponse = &Response{200, "Ok"}
+
+func (s *projectService) Start(ctx context.Context, msg *Project) (*Response, error) {
+	log.Debugf("Start %s (v%s)", msg.GetName(), msg.GetVersion())
+	return defaultResponse, nil
+}
+
+func (s *projectService) Status(ctx context.Context, msg *Project) (*Response, error) {
+	log.Debugf("Status of %s (v%s)", msg.GetName(), msg.GetVersion())
+	return defaultResponse, nil
+}
+
+func (s *projectService) Stop(ctx context.Context, msg *Project) (*Response, error) {
+	log.Debugf("Stop %s (v%s)", msg.GetName(), msg.GetVersion())
+	return defaultResponse, nil
+}
